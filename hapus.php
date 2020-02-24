10c0ceba699b7768a99fcfa45d6664d4882e53aa
Irdanz <irdanramadan55.siswa@smkn4bdg.sch.id>
2020-02-24 15:03:34 +0700

    Commit Pertama

---
+++ hapus.php
@@ -0,0 +1,4 @@
+<?php
+echo "ini seharusnya berfungsi untuk menghapus data namun tidak dapat digunakan";
+echo "karena tidak ada apapun disini selain tulisan";
+?>
---
+++ tambah.php
@@ -0,0 +1,4 @@
+<?php
+echo "ini seharusnya berfungsi untuk menambah data namun tidak dapat digunakan";
+echo "karena tidak ada apapun disini selain tulisan";
+?>
